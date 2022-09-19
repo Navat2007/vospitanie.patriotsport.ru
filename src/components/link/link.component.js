b20f28e72504032f76f185df5e2c9d9b97ad1fe2
Navat2007 <navat2007@yandex.ru>
2022-09-19 12:51:17 +0300

    add

--- src/components/link/link.component.js
+++ src/components/link/link.component.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './link.component.scss';
 
-const MyLink = ({ text, href, target = "_blank", className = "link" }) => {
+const MyLink = ({text, href, target = "_blank", className = "link"}) => {
 
+    return (
 
-  return (
+        <a className={className}
+           rel="noopener noreferrer nofollow"
+           target={target}
+           href={href}>
+            {text}
+        </a>
 
-    <a className={className}
-      rel="noopener noreferrer nofollow" 
-      target={target} 
-      href={href}>
-      {text}
-    </a>
-
-  );
+    );
 };
 
 export default MyLink;
